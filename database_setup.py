22b24f953a7eb1f3d80564d034ccf8fb4c101440
Chike Ezeh <ezeh.chike@gmail.com>
2017-10-30 07:40:54 -0500

    add the class structure for the database

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python
+# sqlalchemy is an ORM (Object-Relational Mapping) module that
+# we will use to query our database
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# class for the category
+
+
+class Category(Base):
+    pass
+
+# class for the item
+
+
+class Item(Base):
+    pass
+
+
+# class for the users
+
+class User(Base):
+    pass
