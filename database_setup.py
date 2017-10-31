b6cc7b5849a012e2202152d31923ba8ef366932a
Chike Ezeh <ezeh.chike@gmail.com>
2017-10-30 20:11:48 -0500

    Create the database schema

--- database_setup.py
+++ database_setup.py
@@ -7,21 +7,45 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+# class for the users
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 # class for the category
 
 
 class Category(Base):
-    pass
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 # class for the item
 
 
 class Item(Base):
-    pass
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    course = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
-# class for the users
+# create the database file
+engine = create_engine('sqlite:///catalog.db')
 
-class User(Base):
-    pass
+
+Base.metadata.create_all(engine)
