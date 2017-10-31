f9d550ed1dd49674a9d1884f2d384dc7da8209a5
Chike Ezeh <ezeh.chike@gmail.com>
2017-10-31 06:57:53 -0500

    add the categories

---
+++ populate_database.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# python file to prepopulate the database
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Category, Base, Item
+
+engine = create_engine('sqlite:///catalog.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+# Make the categories
+# category1 items
+category1 = Category(name="Soccer")
+session.add(category1)
+session.commit()
+
+# category2 items
+category2 = Category(name="Snowboarding")
+session.add(category2)
+session.commit()
+
+# category3 items
+category3 = Category(name="Frisbee")
+session.add(category3)
+session.commit()
+
+# category4 items
+category4 = Category(name="Baseball")
+session.add(category4)
+session.commit()
